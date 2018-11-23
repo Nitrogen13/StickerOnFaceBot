c1cb44b10e26a314d269a9a31912efee3133672e
Timur <minteemer@gmail.com>
2018-11-23 04:40:40 +0300

    Face kekefier

--- src/image_processing.py
+++ src/image_processing.py
@@ -28,7 +28,7 @@ def outer_fit(source, mask, face_box):
     delta_y = delta_len * math.sin(math.radians(face_box.roll))
     delta_x = delta_len * math.cos(math.radians(face_box.roll))
 
-    sign = 1 if math.fabs(face_box.roll ) < 90 else -1
+    sign = 1 if math.fabs(face_box.roll) < 90 else -1
     if horizontal_expand:
         top = face_box.top - sign * delta_y
         left = face_box.left - sign * delta_x
@@ -50,3 +50,56 @@ def memefy(source, mask, faces):
 
     return source.convert("RGB")
 
+
+def rotate_point(x, y, angle):
+    angle = math.radians(angle)
+    cos = math.cos(angle)
+    sin = math.sin(angle)
+
+    rx = x * cos - y * sin
+    ry = x * sin + y * cos
+    return rx, ry
+
+
+def get_cropped_face(source, face_box):
+    # Find center of the face
+    diag_x, diag_y = rotate_point(face_box.width, face_box.height, face_box.roll)
+    center_x, center_y = int(face_box.left + diag_x / 2), int(face_box.top + diag_y / 2)
+    radius = int(math.sqrt(face_box.width ** 2 + face_box.height ** 2) / 2)
+    print("center: %d, %d; radius: %d" % (center_x, center_y, radius))
+
+    # crop around face and rotate
+    crop_coordinates = (center_x - radius, center_y - radius, center_x + radius, center_y + radius)
+    face = source.crop(crop_coordinates).rotate(face_box.roll, expand=1)
+
+    # crop blank spaces after rotation
+    rotation_angle = math.radians(face_box.roll)
+    padding = int(2 * radius * min(abs(math.cos(rotation_angle)), abs(math.sin(rotation_angle))))
+    face = face.crop((padding, padding, face.size[0] - padding, face.size[1] - padding))
+
+    return face
+
+
+def half_paste(left_side, right_side):
+    width, height = left_side.size
+    half_width = width // 2
+
+    half_face = right_side.crop((half_width, 0, width, height))
+    result = left_side.copy()
+    result.paste(half_face, (half_width, 0), half_face)
+    return result
+
+
+def kekefy(source, faces):
+    print("Kekefy the image...")
+    results = []
+
+    boxes = [FaceBox(face, source.size[0], source.size[1]) for face in faces]
+    for box in boxes:
+        face = get_cropped_face(source, box).convert("RGBA")
+        flipped_face = face.transpose(Image.FLIP_LEFT_RIGHT)
+
+        results.append(half_paste(face, flipped_face).convert("RGB"))
+        results.append(half_paste(flipped_face, face).convert("RGB"))
+
+    return results
